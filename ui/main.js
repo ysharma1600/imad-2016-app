82857ab7566c45f87c4d8a6f99680b3b56ddd8d3
unknown <ysharma1600@gmail.com>
2020-04-07 11:40:24 +0530

    Making Madi move when it is clicked on

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
 console.log('Loaded!');
+
+//Change the HTML Text :-
 var element = document.getElementById("main text");
 element.innerHTML = "I have been changed using JavaScript";
+
+
+//Make "madi" move when clicked on :-
+var img=document.getElementById("madi");
+img.onclick=function()
+{
+    img.style.marginLeft = '100px';
+
+}
