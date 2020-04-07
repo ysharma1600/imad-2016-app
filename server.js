6f5e829813b6e595f99b88309de05e6b238a9d6a
unknown <ysharma1600@gmail.com>
2020-04-07 19:12:32 +0530

    Rectified some Errors

--- server.js
+++ server.js
@@ -102,7 +102,11 @@ app.get('/', function (req, res) {
 });
 
 
-
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter + 1;
+  res.send(counter.toString());
+});
 
 
 app.get('/:articleName', function(req,res){
@@ -129,12 +133,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
-var counter=0;
-app.get('/counter', function (req, res) {
-  counter=counter + 1;
-  res.send(counter.toString());
-});
 
 
 
